ceafb7e7b42b53e43bf69cfc7dda831e496952ad
cmwalther <charly.walther@gmail.com>
2018-09-06 15:08:48 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,5 @@
-
-$( document ).ready(function() {
-  
-  alert("document loaded");
-
-    $("#sentiment_analysis").click(function() {
-    $("#task_container").show();
-    $("#task_overview").hide();
-    alert("hi");
-  });
-
-
-
-
   var tasks = {
-    "sentiment analysis" : {
+    "sentiment_analysis" : {
      "instruction" : "Read the text segment and pick the appropriate sentiment",
      "answer_options" : {
         1 : "sad",
@@ -28,5 +14,27 @@ $( document ).ready(function() {
       ]
     }
   }
+  
+  var counter = 0;
+
+$( document ).ready(function() {
+  
+    $("#sentiment_analysis").click(function() {
+      $("#task_container").show();
+      $("#task_overview").hide();
+      
+      // Initialize sentiment quiz
+      $("#task_instructions").text(tasks["sentiment_analysis"]["instruction"]);      
+      set_game("sentiment_analysis", counter);
+      
+  });
+
+function set_game(var task_name, var source_counter){
+  $("#task_source").text(tasks[task_name]["sources"][source_counter][1]; 
+}
+
+
+
+
 });
 
