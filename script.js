114a72269e806d1d1f8ecdd066933ca1dbca58f7
cmwalther <charly.walther@gmail.com>
2018-09-06 12:20:36 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-alert("hi");
+var tasks = {
+  "sentiment analysis" : {
+    "instruction : "Read the text segment and pick the appropriate sentiment",
+    "answer_options" : {
+      1 : "sad",
+      2 : "angry",
+      3 : "happy"
+    }
+    "sources" : [
+      [2, "Your customer service sucks"]
+      [2, "You suck"]
+      [1, "That's unfortunate"]
+      [3, "Wow so exciting"]
+    ]
+  }
+}
