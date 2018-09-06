d64cc2d54821aa8ace429f869acc8c6997f357ca
cmwalther <charly.walther@gmail.com>
2018-09-06 14:39:15 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,29 @@
 
-$.("#sentiment_analysis").click(function() {
-  $.("#task_container").show();
-  $.("#task_overview").hide();
-  alert("hi");
-});
+$( document ).ready(function() {
+
+  $.("#sentiment_analysis").click(function() {
+    $.("#task_container").show();
+    $.("#task_overview").hide();
+    alert("hi");
+  });
 
 
 
 
-var tasks = {
-  "sentiment analysis" : {
-    "instruction" : "Read the text segment and pick the appropriate sentiment",
-    "answer_options" : {
-      1 : "sad",
-      2 : "angry",
-      3 : "happy"
+  var tasks = {
+    "sentiment analysis" : {
+     "instruction" : "Read the text segment and pick the appropriate sentiment",
+     "answer_options" : {
+        1 : "sad",
+       2 : "angry",
+       3 : "happy"
+      }
+      "sources" : [
+       [2, "Your customer service sucks"]
+       [2, "You suck"]
+       [1, "That's unfortunate"]
+       [3, "Wow so exciting"]
+      ]
     }
-    "sources" : [
-      [2, "Your customer service sucks"]
-      [2, "You suck"]
-      [1, "That's unfortunate"]
-      [3, "Wow so exciting"]
-    ]
-  }
+  });
 }
